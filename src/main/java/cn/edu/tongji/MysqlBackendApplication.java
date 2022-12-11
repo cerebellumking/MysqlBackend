e3756dc4d21a84ea90deab562504b0992595ab6b
cerebellumking <1805219550@qq.com>
2022-12-11 19:18:46 +0800

    update .gitignore

---
+++ src/main/java/cn/edu/tongji/MysqlBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.tongji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlBackendApplication.class, args);
+    }
+
+}
